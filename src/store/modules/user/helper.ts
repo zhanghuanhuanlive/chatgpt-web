b89753b4880731ef23477c60628c03e68aa9119b
zhanghuanhuanlive <50442862@qq.com>
2023-11-02 20:33:08 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -17,7 +17,7 @@ export function defaultSetting(): UserState {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
       name: 'ChatGLM',
-      description: '" class="text-blue-500" target="_blank" >GitHub</a>',
+      description: 'AI助手',
     },
   }
 }
