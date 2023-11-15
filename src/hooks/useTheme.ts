585a5913def441f58396e9ecb4a30bb2f61f64eb
zhanghuanhuanlive <50442862@qq.com>
2023-11-15 17:30:17 +0800

    feat: 默认设置为dark模式，隐藏一些设置选项

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,20 +1,19 @@
 import type { GlobalThemeOverrides } from 'naive-ui'
-import { computed } from 'vue'
+import { computed, watch } from 'vue'
 import { darkTheme, useOsTheme } from 'naive-ui'
 import { useAppStore } from '@/store'
 
 export function useTheme() {
-  document.documentElement.classList.add('dark')
-
   const appStore = useAppStore()
 
   const OsTheme = useOsTheme()
 
   const isDark = computed(() => {
-    if (appStore.theme === 'auto')
-      return OsTheme.value === 'dark'
-    else
-      return appStore.theme === 'dark'
+    return true
+    // if (appStore.theme === 'auto')
+    //   return OsTheme.value === 'dark'
+    // else
+    //   return appStore.theme === 'dark'
   })
 
   const theme = computed(() => {
@@ -30,16 +29,16 @@ export function useTheme() {
     return {}
   })
 
-  // watch(
-  //   () => isDark.value,
-  //   (dark) => {
-  //     if (dark)
-  //       document.documentElement.classList.add('dark')
-  //     else
-  //       document.documentElement.classList.remove('dark')
-  //   },
-  //   { immediate: true },
-  // )
+  watch(
+    () => isDark.value,
+    (dark) => {
+      if (dark)
+        document.documentElement.classList.add('dark')
+      else
+        document.documentElement.classList.remove('dark')
+    },
+    { immediate: true },
+  )
 
   return { theme, themeOverrides }
 }
